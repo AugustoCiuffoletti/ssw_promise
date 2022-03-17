e0410d82e38b305ef2d52bbdeff187292fe66bb5
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-17 17:56:24 +0100

    1 file modified, 1 added, 1 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# js-sswawait-a8uxr6
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sswawait-a8uxr6)
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import "./style.css";
-
 const appDiv = document.getElementById("app");
 const outDiv = document.getElementById("output");
 appDiv.innerHTML = `<h1>Un esempio di <i>promise<i></h1>`;
+// Una funzione che restituisce una promise che si risolve dopo
+// un intervallo di tempo definito
 function sleep(ms) {
   return new Promise( resolve => setTimeout(() => resolve(), ms) ) ;
 }
+// Prima variazione: funzione resolve esplicita
 function resolve1() {
   outDiv.innerHTML += "Resolve esplicita";
 }
-
-sleep(3000, "Fine attesa <br>").then(resolve1);
-sleep(3000, "Fine attesa <br>").then(() => console.log("Resolve anonima"));
-new Promise(resolve => setTimeout(() => resolve(), 3000)).then(() => console.log("Promise anonima")); 
+sleep(3000).then(resolve1);
+// Seconda variazione: funzione resolve anonima (definita arrow)
+sleep(3000)
+  .then(() => console.log("Resolve anonima"));
+// Terza variazione: promise e resolve entrambe anonime
+new Promise(resolve => setTimeout(() => resolve(), 3000))
+  .then(() => console.log("Promise anonima")); 
