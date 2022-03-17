e059f6639b2482452667ca9e34d4181b7854445f
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-17 14:45:10 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 const outDiv = document.getElementById("output");
 appDiv.innerHTML = `<h1>Un esempio di <i>promise<i></h1>`;
-
 function sleep(ms, txt) {
   return new Promise( resolve => {
     setTimeout(() => resolve(txt), ms);
   });
 }
-
 function resolve1(txt) {
   outDiv.innerHTML += txt;
 }
