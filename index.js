045d0468c43da35ff1726edabbef16f265d418d1
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-17 17:49:50 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 const outDiv = document.getElementById("output");
 appDiv.innerHTML = `<h1>Un esempio di <i>promise<i></h1>`;
-function sleep(ms, txt) {
-  return new Promise( resolve => { setTimeout(() => resolve(txt), ms)); } ;
+function sleep(ms) {
+  return new Promise( resolve => setTimeout(() => resolve(), ms) ) ;
 }
-function resolve1(txt) {
-  outDiv.innerHTML += txt;
+function resolve1() {
+  outDiv.innerHTML += "Resolve esplicita";
 }
 
 sleep(3000, "Fine attesa <br>").then(resolve1);
-sleep(3000, "Fine attesa <br>").then((txt) => console.log(txt));
-new Promise(resolve =>)
+sleep(3000, "Fine attesa <br>").then(() => console.log("Resolve anonima"));
+new Promise(resolve => setTimeout(() => resolve(), 3000)).then(() => console.log("Promise anonima")); 
